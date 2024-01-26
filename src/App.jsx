9c2b92e4ebfff2999417672c011b9b014ac95fd0
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-01-26 23:42:56 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,26 @@ import rootReducer from "./reducers/index";
 import { fetchPropertyList } from "./actions/fetch.action";
 import HouseList from "./components/HouseList";
 
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: true,
-});
+// const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: true,
+// });
 
-store.dispatch(fetchPropertyList());
+// store.dispatch(fetchPropertyList());
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/" element={<Home />}>
-            <Route index element={<HouseList />} />
-            <Route path="/:title" element={<HouseDetail />} />
-            <Route path="*" replace element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </Router>
-    </Provider>
+    // <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<HouseList />} />
+          <Route path="/:title" element={<HouseDetail />} />
+          <Route path="*" replace element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </Router>
+    // </Provider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+//REDUX
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
+import { fetchPropertyList } from "./actions/fetch.action";
+import HouseList from "./components/HouseList";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
